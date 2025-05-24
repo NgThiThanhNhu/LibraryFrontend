5019317b194d6b648dbf3f62320467b409fc1ffc
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-05-24 17:47:26 +0700

    add table

--- src/layout/PublisherTable.tsx
+++ src/layout/PublisherTable.tsx
@@ -1,17 +1,17 @@
 
-import { DataGrid} from '@mui/x-data-grid';
-import type { GridColDef, GridRowSelectionModel} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
+import type { GridColDef, GridRowSelectionModel } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import type { PublisherResponse } from '../response/publisherResponse';
 
 
-type  Props = {
-  data : PublisherResponse[],
+type Props = {
+  data: PublisherResponse[],
   reload: boolean,
-  
+  onSelectionChange: (selectionIds: string[]) => void
 }
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', flex: 1  },
+  { field: 'id', headerName: 'ID', flex: 1 },
   { field: 'publisherName', headerName: 'Tên nhà xuất bản', flex: 1 },
   { field: 'email', headerName: 'Email', width: 280 },
   {
@@ -23,28 +23,32 @@ const columns: GridColDef[] = [
     field: 'address',
     headerName: 'Địa chỉ',
     flex: 1,
-    
+
   },
 ];
 
 
 const paginationModel = { page: 0, pageSize: 5 };
 
-export default function PublisherTable({data, reload
-  }: Props) {
-  
-  
+export default function PublisherTable({ data, reload, onSelectionChange
+}: Props) {
+
+
   return (
     <Paper sx={{ height: 400, width: '100%' }}>
       <DataGrid
         rows={data}
-        loading = {reload}
-        getRowId={(row)=>row.id} //đảm bảo mỗi hàng có mỗi id duy nhất
+        loading={reload}
+        getRowId={(row) => row.id} //đảm bảo mỗi hàng có mỗi id duy nhất
         columns={columns}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
-        
+
+        onRowSelectionModelChange={(selection) => {
+          const stringIds = Array.from(selection.ids).map((id) => String(id));
+          onSelectionChange(stringIds);
+        }}
         sx={{ border: 0 }}
       />
     </Paper>
