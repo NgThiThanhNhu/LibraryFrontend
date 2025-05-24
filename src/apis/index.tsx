d43a9f1efae76d1e5ea8d582b42c77ad202a0b3f
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-05-24 17:47:12 +0700

    update api

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -3,20 +3,24 @@ import { BASE_URL } from "./config";
 import type { PublisherRequest } from "../request/publisherRequest";
 
 const PublisherApi = {
-    addPublisher: async (data: PublisherRequest)=>{
+    addPublisher: async (data: PublisherRequest) => {
         const response = await axios.post(`${BASE_URL}/api/Publisher/AddPublisher`, data);
         return response.data;
     },
-    getAllPublisher: async ()=>{
+    getAllPublisher: async () => {
         const response = await axios.get(`${BASE_URL}/api/Publisher/GetAllPublisher`)
         return response.data;
     },
-    updatePublisher: async (id: string, data: PublisherRequest, )=>{
+    updatePublisher: async (id: string, data: PublisherRequest,) => {
         const response = await axios.post(`${BASE_URL}/api/Publisher/UpdatePublisher/${id}`, data)
         return response.data;
     },
-    getPublisherById: async (id: string)=>{
+    getPublisherById: async (id: string) => {
         const response = await axios.get(`${BASE_URL}/api/Publisher/GetPublisherById/${id}`)
+        return response;
+    },
+    deletePublisher: async (id: string) => {
+        const response = await axios.post(`${BASE_URL}/api/Publisher/DeletePublisher/${id}`)
         return response;
     }
 }
