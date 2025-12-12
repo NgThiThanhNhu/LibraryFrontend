807070bee506ebd622f2972733074c23757d94b4
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-12-12 09:10:28 +0700

    ThanhNhu: add user route

--- src/routes/ClientUserRoutes.tsx
+++ src/routes/ClientUserRoutes.tsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-
-
 import BookListUserHomePage from "../pages/UserHomePage";
-
 import MyBookList from "../pages/MyBookList";
 import BorrowingStatusPage from "../pages/BorrowingStatusPage";
 import { BookDetailPage } from "../pages/BookDetailPage";
 import { UserRoute } from "../route/UserRoute";
+import BookFileReadingPage from "../pages/BookFileReadingPage";
 
 export const ClientUserRoutes = () => {
     return (
@@ -15,6 +13,7 @@ export const ClientUserRoutes = () => {
             <Route path="/books/:slug" element={<UserRoute><BookDetailPage /></UserRoute>} />
             <Route path="/mybooklist" element={<UserRoute><MyBookList /></UserRoute>} />
             <Route path="/borrowingstatus" element={<UserRoute><BorrowingStatusPage /></UserRoute>} />
+            <Route path="/read/:bookId" element={<BookFileReadingPage />} />
         </Routes>
     );
 };
